0a03e588ae4e9a28ce7aa42358411fae826e7aaa
Deborah_Maia <deborahmaia48@gmail.com>
2024-09-30 02:42:33 +0000

    atualização da transição de página para adicionar uma tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -46,72 +46,90 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     return Scaffold(
-      drawer: Drawer(
-        width: 200,
-        child: Column(
-          children: [
-            ListTile(
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              leading: const Icon(Icons.person, color: Color(0xffd98baf)),
-              title: const Text('Perfil',
-                  style: TextStyle(
-                    color: Color(0xffbf567d),
-                    fontSize: 20,
-                  )),
-              onTap: () {
-                setState(() {
-                  selectedIndex = 0;
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-            ListTile(
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              leading: const Icon(Icons.book, color: Color(0xffd98baf)),
-              title: const Text('Meu Planner',
-                  style: TextStyle(
-                    color: Color(0xffbf567d),
-                    fontSize: 20,
-                  )),
-              onTap: () {
-                setState(() {
-                  selectedIndex = 1;
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+        drawer: Drawer(
+          width: 200,
+          child: Column(
+            children: [
+              ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                leading: const Icon(Icons.person, color: Color(0xffd98baf)),
+                title: const Text('Perfil',
+                    style: TextStyle(
+                      color: Color(0xffbf567d),
+                      fontSize: 20,
+                    )),
+                onTap: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                leading: const Icon(Icons.book, color: Color(0xffd98baf)),
+                title: const Text('Meu Planner',
+                    style: TextStyle(
+                      color: Color(0xffbf567d),
+                      fontSize: 20,
+                    )),
+                onTap: () {
+                  setState(() {
+                    selectedIndex = 1;
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Image.asset(
-              'assets/imagens/logo_pixie.png',
-              fit: BoxFit.contain,
-              height: 50,
-            ),
-          ],
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset(
+                'assets/imagens/logo_pixie.png',
+                fit: BoxFit.contain,
+                height: 50,
+              ),
+            ],
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: page,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //Página de criação de tarefa
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const NewPage()),
-          );
-        },
-        backgroundColor: Color(0xffbf567d),
-        foregroundColor: Colors.white,
-        shape: const CircleBorder(),
-        child: const Icon(Icons.add), // Ícone de "Adicionar"
-      ),
-    );
+        body: page,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Navegar para a página de nova tarefa com a transição personalizada
+            Navigator.of(context).push(
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    const NewPage(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  const begin =
+                      Offset(0.0, 1.0); // Inicia fora da tela, embaixo
+                  const end = Offset.zero; // Fim no centro da tela
+                  const curve = Curves.ease;
+
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+
+                  return SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  );
+                },
+              ),
+            );
+          },
+          backgroundColor: Color(0xffbf567d),
+          foregroundColor: Colors.white,
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add), // Ícone de "Adicionar"
+        ));
   }
 }
 
@@ -147,6 +165,12 @@ class PlannerPage extends StatelessWidget {
     int todayIndex = days.indexWhere((day) =>
         day.day == now.day && day.month == now.month && day.year == now.year);
 
+    // Cria o ScrollController e define a posição inicial
+    final ScrollController scrollController = ScrollController(
+      initialScrollOffset:
+          todayIndex * 75.0, // Deslocamento inicial baseado no dia
+    );
+
     return Column(
       children: [
         Center(
@@ -159,11 +183,9 @@ class PlannerPage extends StatelessWidget {
         Container(
           height: 60,
           child: ListView.builder(
+            controller: scrollController, // Vincula o ScrollController
             scrollDirection: Axis.horizontal, // Rolagem horizontal
             itemCount: days.length, // Número de dias
-            controller: ScrollController(
-                initialScrollOffset:
-                    todayIndex * 75.0), // Ajusta a rolagem para o dia atual
             itemBuilder: (context, index) {
               DateTime day = days[index];
               bool isToday = day.day == now.day &&
@@ -172,7 +194,7 @@ class PlannerPage extends StatelessWidget {
 
               return GestureDetector(
                 onTap: () {
-                  // modificar a página home
+                  // Modificar a página home ou qualquer outra ação ao tocar no dia
                 },
                 child: Container(
                   width: 50,
